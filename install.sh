02f8c26066c330dba8a2e205f9d9321152d21f04
oWinterzz <79472940+oWinterzz@users.noreply.github.com>
2021-03-29 23:39:18 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+echo "vWinter's Arch Installer"
+read -p 'Are you connected to internet? [y/N]: ' neton
+if ! [ $neton = 'y' ] && ! [ $neton = 'Y' ]
+then 
+    echo "Connect to internet to continue..."
+    exit
+fi
+echo "This script will create and format the partitions as follows:"
+echo "/dev/sda1 - 512Mib will be mounted as /boot/efi"
+echo "/dev/sda2 - 8GiB will be used as swap"
+echo "/dev/sda3 - rest of space will be mounted as /"
+read -p 'Continue? [y/N]: ' fsok
+if ! [ $fsok = 'y' ] && ! [ $fsok = 'Y' ]
+then 
+    echo "Edit the script to continue..."
+    exit
+fi
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +512M # 512 MB boot parttion
+  n # new partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+  +8G # 8 GB swap parttion
+  n # new partition
+  p # primary partition
+  3 # partion number 3
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  a # make a partition bootable
+  1 # bootable partition is partition 1 -- /dev/sda1
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+mkfs.ext4 /dev/sda3
+mkfs.fat -F32 /dev/sda1
+timedatectl set-ntp true
+pacman -Syy
+mount /dev/sda3 /mnt
+mkdir -pv /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+mkswap /dev/sda2
+swapon /dev/sda2
+echo "Starting install.."
+echo "Installing Arch Linux, KDE with Konsole and Dolphin and GRUB2 as bootloader" 
+pacstrap /mnt base base-devel zsh grml-zsh-config grub os-prober intel-ucode efibootmgr dosfstools freetype2 fuse2 mtools iw wpa_supplicant dialog mesa xf86-video-intel xorg xorg-server deepin deepin-extra
+genfstab -U /mnt >> /mnt/etc/fstab
+cp -rfv post-install.sh /mnt/root
+chmod a+x /mnt/root/post-install.sh
+echo "After chrooting into newly installed OS, please run the post-install.sh by executing ./post-install.sh"
+echo "Press any key to chroot..."
+read tmpvar
+arch-chroot /mnt /bin/bash
+echo "If post-install.sh was run succesfully, you will now have a fully working bootable Arch Linux system installed."
+echo "The only thing left is to reboot into the new system."
+echo "Press any key to reboot or Ctrl+C to cancel..."
+read tmpvar
+reboot
---
+++ post-install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo "vWinter's Arch Configurator"
+ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
+hwclock --systohc
+sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.1.1 arch.localdomain  arch" >> /etc/hosts
+mkinitcpio -P
+passwd
+echo "greeter-session=lightdm-deepin-greeter" >> /etc/lightdm/lightdm.conf
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
+grub-mkconfig -o /boot/grub/grub.cfg
+useradd -m -G wheel,power,input,storage,uucp,network -s /usr/bin/zsh winter
+sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
+echo "Set password for new user winter"
+passwd winter
+systemctl start lightdm.service
+systemctl enable lightdm.service
+systemctl enable NetworkManager.service
+echo "Configuration done. You can now exit chroot."
